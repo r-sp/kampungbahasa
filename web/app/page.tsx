93785de9f84936fce7d168eca14d693659ac9290
r-sp <36327242+r-sp@users.noreply.github.com>
2024-09-21 22:27:26 +0700

    add icons as component

--- web/app/page.tsx
+++ web/app/page.tsx
@@ -1,5 +1,12 @@
 import type { Metadata } from "next";
 import Logo from "./logo";
+import {
+  HomeIcon,
+  SearchIcon,
+  CourseIcon,
+  ActivityIcon,
+  ProfileIcon,
+} from "~/components/icons";
 
 export const metadata: Metadata = {
   title: "Kampung Bahasa",
@@ -11,6 +18,13 @@ export default function Homepage() {
     <>
       <header>
         <Logo className="flex items-center justify-center h-10 w-10 rounded-xl bg-cn-100" />
+        <nav className="flex items-center justify-between p-4 fixed left-0 bottom-0 right-0 z-40">
+          <HomeIcon />
+          <SearchIcon />
+          <CourseIcon />
+          <ActivityIcon />
+          <ProfileIcon />
+        </nav>
       </header>
       <main>
         <article>
