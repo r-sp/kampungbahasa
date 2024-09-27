fbf9892bad8cd3a2036f927406df93a5abf085f9
r-sp <36327242+r-sp@users.noreply.github.com>
2024-09-27 22:15:26 +0700

    add test icon

--- web/app/(landing)/page.tsx
+++ web/app/(landing)/page.tsx
@@ -1,5 +1,6 @@
 import { type Metadata } from "next";
 import { Prose } from "~/components/Prose";
+import TestIcon from "./test-icon";
 
 export const metadata: Metadata = {
   title: "Kampung Bahasa",
@@ -9,6 +10,7 @@ export const metadata: Metadata = {
 export default function HomePage() {
   return (
     <Prose className="px-4 py-6">
+      <TestIcon />
       <h1>A place to learn, practice and improve your speaking</h1>
       <p>
         <strong>Are you seeking to elevate your speaking abilities?</strong> Whether you're
