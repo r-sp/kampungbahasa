bff18c5f8f195af67b2f528033b7249745ff4abc
r-sp <36327242+r-sp@users.noreply.github.com>
2024-09-30 04:16:49 +0700

    preview adaptive layout and typography (#33)
    * add preview mobile first navigation
    
    * add root component to children
    
    * appy custom tailwind config and styles
    
    * add preview typography with prose

--- web/components/Header.tsx
+++ web/components/Header.tsx
@@ -4,46 +4,62 @@ import Logo from "./logo";
 export default function Header() {
   return (
     <header>
-      <nav className="flex items-center h-16 px-4">
-        <Link href="/" className="contents">
-          <Logo variant="gradient" />
-          <span className="text-2xl text-cn-900 rounded-lg font-semibold tracking-tight mx-3">
-            Kampung Bahasa
-          </span>
-        </Link>
-        <div className="fixed inset-0 opacity-0 pointer-events-none z-10" role="none">
-          <div className="flex flex-col items-center h-screen bg-cn-100" role="none">
-            <Link
-              href="/course"
-              className="inline-flex items-center text-base text-cn-600 hover:text-cn-800 rounded-lg font-medium h-12 px-3"
-              tabIndex={-1}
+      <div
+        className="bg-pare-200 shadow-1dp fixed left-0 right-0 top-0 z-10 flex items-center gap-3 sm:h-16 sm:px-4"
+        role="none"
+      >
+        <div
+          className="sm:contents sm-r:flex sm-r:items-center sm-r:gap-3 sm-r:h-16 sm-r:pl-4 sm-r:pr-3 sm-r:min-w-full"
+          role="none"
+        >
+          <Link href="/" className="contents">
+            <Logo variant="gradient" aria-hidden="true" />
+            <span className="text-cn-800 text-2xl font-semibold tracking-tight">
+              Kampung Bahasa
+            </span>
+          </Link>
+          <div aria-hidden="true" className="sm:hidden sm-r:flex-grow" role="none"></div>
+          <button className="toggle-menu sm:hidden">
+            <svg
+              className="block w-8 h-8"
+              viewBox="0 0 32 32"
+              fill="none"
+              xmlns="http://www.w3.org/2000/svg"
             >
-              <span>Course</span>
-            </Link>
-            <Link
-              href="/curriculum"
-              className="inline-flex items-center text-base text-cn-600 hover:text-cn-800 rounded-lg font-medium h-12 px-3"
-              tabIndex={-1}
-            >
-              <span>Curriculum</span>
-            </Link>
-            <Link
-              href="/ecosystem"
-              className="inline-flex items-center text-base text-cn-600 hover:text-cn-800 rounded-lg font-medium h-12 px-3"
-              tabIndex={-1}
-            >
-              <span>Ecosystem</span>
-            </Link>
-            <Link
-              href="/schedule"
-              className="inline-flex items-center text-base text-cn-600 hover:text-cn-800 rounded-lg font-medium h-12 px-3"
-              tabIndex={-1}
-            >
-              <span>Schedule</span>
-            </Link>
-          </div>
+              <path
+                className="top"
+                fillRule="evenodd"
+                clipRule="evenodd"
+                d="M4 7C4 6.44772 4.44772 6 5 6H27C27.5523 6 28 6.44772 28 7C28 7.55228 27.5523 8 27 8H5C4.44772 8 4 7.55228 4 7Z"
+                fill="currentColor"
+              ></path>
+              <path
+                className="mid"
+                fillRule="evenodd"
+                clipRule="evenodd"
+                d="M4 16C4 15.4477 4.44772 15 5 15H27C27.5523 15 28 15.4477 28 16C28 16.5523 27.5523 17 27 17H5C4.44772 17 4 16.5523 4 16Z"
+                fill="currentColor"
+              ></path>
+              <path
+                className="bot"
+                fillRule="evenodd"
+                clipRule="evenodd"
+                d="M4 25C4 24.4477 4.44772 24 5 24H27C27.5523 24 28 24.4477 28 25C28 25.5523 27.5523 26 27 26H5C4.44772 26 4 25.5523 4 25Z"
+                fill="currentColor"
+              ></path>
+            </svg>
+          </button>
         </div>
-      </nav>
+        <div className="sm:contents sm-r:hidden" role="none">
+          <nav className="text-cn-700 sm:contents">
+            <Link href="/course">Course</Link>
+            <Link href="/curriculum">Curriculum</Link>
+            <Link href="/ecosystem">Ecosystem</Link>
+            <Link href="/schedule">Schedule</Link>
+          </nav>
+        </div>
+      </div>
+      <div className="h-16 pointer-events-none" role="none"></div>
     </header>
   );
 }
