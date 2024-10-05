067b6bc60c19ef3ae62aeecf1a559fd639c527db
r-sp <36327242+r-sp@users.noreply.github.com>
2024-10-05 21:44:17 +0700

    add logo and banner components

--- web/components/client/Footer.tsx
+++ web/components/client/Footer.tsx
@@ -1,14 +1,14 @@
 import Link from "next/link";
-import { Logo } from "~/assets/Logo";
+import { Banner } from "~/assets/Logo";
 
-export default function Header() {
+export default function Footer() {
   return (
-    <header>
+    <footer>
       <nav className="flex items-center h-14 mx-auto max-w-3xl ps-4 px-4">
         <Link href="/" className="inline-block">
-          <Logo className="block h-8 w-8" />
+          <Banner className="block h-8 w-auto" />
         </Link>
       </nav>
-    </header>
+    </footer>
   );
 }
