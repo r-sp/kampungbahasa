72ee1b5b85c26c9519a7d2763edb444309c99c9e
r-sp <36327242+r-sp@users.noreply.github.com>
2024-10-06 20:41:37 +0700

    use padding top and bottom instead of padding inline start

--- web/components/client/Header.tsx
+++ web/components/client/Header.tsx
@@ -4,7 +4,7 @@ import { Logo } from "~/assets/Logo";
 export default function Header() {
   return (
     <header>
-      <nav className="mx-auto flex h-14 max-w-3xl items-center px-4 ps-4">
+      <nav className="mx-auto flex h-14 max-w-3xl items-center px-4 py-4">
         <Link href="/" className="inline-block">
           <Logo className="block h-8 w-8" />
         </Link>
