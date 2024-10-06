baa3b99ae355c638d62d8acd73cd9e696730883e
r-sp <36327242+r-sp@users.noreply.github.com>
2024-10-06 15:16:09 +0700

    chore: add eslint-config-prettier

--- web/eslint.config.mjs
+++ web/eslint.config.mjs
@@ -1,7 +1,9 @@
 import eslint from "@eslint/js";
 import tseslint from "typescript-eslint";
+import eslintConfigPrettier from "eslint-config-prettier";
 
 export default tseslint.config(
+  eslintConfigPrettier,
   eslint.configs.recommended,
   ...tseslint.configs.recommended,
   {
