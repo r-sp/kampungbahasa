6b665f4caf6363968aaf03e534fdf64a4d7b4041
r-sp <36327242+r-sp@users.noreply.github.com>
2024-10-08 22:46:36 +0700

    add new icon variant

--- web/assets/icons.ts
+++ web/assets/icons.ts
@@ -2,5 +2,18 @@ import IconHome from "./icon/home";
 import IconSearch from "./icon/search";
 import IconFavorite from "./icon/favorite";
 import IconPerson from "./icon/person";
+import IconBag from "./icon/bag";
+import IconCalendar from "./icon/calendar";
+import IconStar from "./icon/star";
+import IconLibrary from "./icon/library";
 
-export { IconHome, IconSearch, IconFavorite, IconPerson };
+export {
+  IconHome,
+  IconSearch,
+  IconFavorite,
+  IconPerson,
+  IconBag,
+  IconCalendar,
+  IconStar,
+  IconLibrary,
+};
