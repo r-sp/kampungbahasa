769ba7212529d3d08a97a693c43374017d5abecb
r-sp <36327242+r-sp@users.noreply.github.com>
2024-10-10 14:39:10 +0700

    add color section and invert styles

---
+++ web/app/playground/section/color.tsx
@@ -0,0 +1,75 @@
+export default function SectionColor() {
+  return (
+    <section aria-labelledby="color">
+      <h2 id="color">Color</h2>
+      <div className="flex select-none flex-wrap">
+        <div className="bg-kb-50 inline-flex px-8 py-4">
+          <span className="invert-auto">kb-50</span>
+        </div>
+        <div className="bg-kb-100 inline-flex px-8 py-4">
+          <span className="invert-auto">kb-100</span>
+        </div>
+        <div className="bg-kb-200 inline-flex px-8 py-4">
+          <span className="invert-auto">kb-200</span>
+        </div>
+        <div className="bg-kb-300 inline-flex px-8 py-4">
+          <span className="invert-auto">kb-300</span>
+        </div>
+        <div className="bg-kb-400 inline-flex px-8 py-4">
+          <span className="invert-auto">kb-400</span>
+        </div>
+        <div className="bg-kb-500 inline-flex px-8 py-4">
+          <span className="invert-auto">kb-500</span>
+        </div>
+        <div className="bg-kb-600 inline-flex px-8 py-4">
+          <span className="invert-auto">kb-600</span>
+        </div>
+        <div className="bg-kb-700 inline-flex px-8 py-4">
+          <span className="invert-auto">kb-700</span>
+        </div>
+        <div className="bg-kb-800 inline-flex px-8 py-4">
+          <span className="invert-auto">kb-800</span>
+        </div>
+        <div className="bg-kb-900 inline-flex px-8 py-4">
+          <span className="invert-auto">kb-900</span>
+        </div>
+        <div className="bg-kb-950 inline-flex px-8 py-4">
+          <span className="invert-auto">kb-950</span>
+        </div>
+        <div className="inline-flex bg-neutral-50 px-8 py-4">
+          <span className="invert-auto">neutral-50</span>
+        </div>
+        <div className="inline-flex bg-neutral-100 px-8 py-4">
+          <span className="invert-auto">neutral-100</span>
+        </div>
+        <div className="inline-flex bg-neutral-200 px-8 py-4">
+          <span className="invert-auto">neutral-200</span>
+        </div>
+        <div className="inline-flex bg-neutral-300 px-8 py-4">
+          <span className="invert-auto">neutral-300</span>
+        </div>
+        <div className="inline-flex bg-neutral-400 px-8 py-4">
+          <span className="invert-auto">neutral-400</span>
+        </div>
+        <div className="inline-flex bg-neutral-500 px-8 py-4">
+          <span className="invert-auto">neutral-500</span>
+        </div>
+        <div className="inline-flex bg-neutral-600 px-8 py-4">
+          <span className="invert-auto">neutral-600</span>
+        </div>
+        <div className="inline-flex bg-neutral-700 px-8 py-4">
+          <span className="invert-auto">neutral-700</span>
+        </div>
+        <div className="inline-flex bg-neutral-800 px-8 py-4">
+          <span className="invert-auto">neutral-800</span>
+        </div>
+        <div className="inline-flex bg-neutral-900 px-8 py-4">
+          <span className="invert-auto">neutral-900</span>
+        </div>
+        <div className="inline-flex bg-neutral-950 px-8 py-4">
+          <span className="invert-auto">neutral-950</span>
+        </div>
+      </div>
+    </section>
+  );
+}
