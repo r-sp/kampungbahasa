0217ee1df465f60f1ba5ce7732bc4ac091bc22b3
r-sp <36327242+r-sp@users.noreply.github.com>
2024-10-10 14:45:35 +0700

    add inverted preview colors (#51)

--- web/app/playground/page.tsx
+++ web/app/playground/page.tsx
@@ -1,6 +1,7 @@
 import Link from "next/link";
 import { Prose } from "~/components/server/Prose";
 
+import SectionColor from "./section/color";
 import SectionIconVariant from "./section/icon-variant";
 import SectionIconOutline from "./section/icon-outline";
 
@@ -8,6 +9,7 @@ export default function PlaygroundPage() {
   return (
     <Prose className="mx-auto" aria-labelledby="headline">
       <h1 id="headline">Playground</h1>
+      <SectionColor />
       <SectionIconVariant />
       <SectionIconOutline />
       <hr />
