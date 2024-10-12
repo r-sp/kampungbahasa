c3afd0b84991fb286245d5c2184535297b1acfad
r-sp <36327242+r-sp@users.noreply.github.com>
2024-10-12 17:33:16 +0700

    add state for theme context

--- web/components/context.ts
+++ web/components/context.ts
@@ -1,4 +1,11 @@
 import { createContext } from "react";
 
-export const ThemeContext = createContext<"dark" | "light" | "system">("system");
+export const ThemeContext = createContext<{
+  theme: "dark" | "light" | "system";
+  setTheme: React.Dispatch<React.SetStateAction<"dark" | "light" | "system">>;
+}>({
+  theme: "system",
+  setTheme: () => {},
+});
+
 export const AuthContext = createContext(null);
