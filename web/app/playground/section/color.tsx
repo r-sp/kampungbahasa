5f5deb2efc5da0c4812a8721c06ef3ec466fecf0
r-sp <36327242+r-sp@users.noreply.github.com>
2024-10-14 16:05:37 +0700

    add color state sample

--- web/app/playground/section/color.tsx
+++ web/app/playground/section/color.tsx
@@ -34,6 +34,29 @@ export default function SectionColor() {
         <BoxColor className="bg-slate-900" text="900" />
         <BoxColor className="bg-slate-950" text="950" />
       </ul>
+      <ul className="flex max-w-full gap-x-1 overflow-x-auto">
+        <BoxColor className="bg-state-100 hover:bg-state-200 active:bg-state-300" text="100" />
+        <BoxColor className="bg-state-200" text="200" />
+        <BoxColor className="bg-state-300" text="300" />
+        <BoxColor className="bg-state-400 hover:bg-state-500 active:bg-state-600" text="400" />
+        <BoxColor className="bg-state-500" text="500" />
+        <BoxColor className="bg-state-600" text="600" />
+        <BoxColor className="bg-state-700 hover:bg-state-800 active:bg-state-900" text="700" />
+        <BoxColor className="bg-state-800" text="800" />
+        <BoxColor className="bg-state-900" text="900" />
+      </ul>
+      <p className="text-state-100 hover:text-state-200 active:text-state-300">
+        By leveraging these resources and practicing regularly, you can significantly enhance
+        your speaking skills and boost your confidence.
+      </p>
+      <p className="text-state-400 hover:text-state-500 active:text-state-600">
+        By leveraging these resources and practicing regularly, you can significantly enhance
+        your speaking skills and boost your confidence.
+      </p>
+      <p className="text-state-700 hover:text-state-800 active:text-state-900">
+        By leveraging these resources and practicing regularly, you can significantly enhance
+        your speaking skills and boost your confidence.
+      </p>
     </section>
   );
 }
