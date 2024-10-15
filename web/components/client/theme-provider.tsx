eb387c445d155a3480a1f329d4b46daf95f8cc3b
r-sp <36327242+r-sp@users.noreply.github.com>
2024-10-15 22:43:46 +0700

    global theme (#66)

--- web/components/client/theme-provider.tsx
+++ web/components/client/theme-provider.tsx
@@ -1,10 +1,30 @@
 "use client";
 
-import { useState, useMemo } from "react";
+import { useEffect, useState, useMemo } from "react";
 import { ThemeContext } from "~/components/context";
 
 export default function ThemeProvider(props: { children: React.ReactNode }) {
   const [theme, setTheme] = useState<"dark" | "light" | "system">("system");
+
+  useEffect(() => {
+    let local;
+    if (typeof window !== "undefined") {
+      try {
+        local = localStorage.getItem("theme");
+      } catch (e) {
+        console.error(e);
+      }
+    }
+
+    if (local === "dark") {
+      if (theme !== "dark") setTheme("dark");
+    } else if (local === "light") {
+      if (theme !== "light") setTheme("light");
+    } else {
+      if (theme !== "system") setTheme("system");
+    }
+  }, [theme, setTheme]);
+
   const switchTheme = useMemo(() => ({ theme, setTheme }), [theme, setTheme]);
 
   return <ThemeContext.Provider value={switchTheme}>{props.children}</ThemeContext.Provider>;
