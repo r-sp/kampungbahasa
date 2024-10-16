7bba9b86b9d6b48c72da0a33753a03446b9aaee1
r-sp <36327242+r-sp@users.noreply.github.com>
2024-10-16 15:51:10 +0700

    real project start here (#69)

--- web/app/playground/section/index.ts
+++
@@ -1,7 +0,0 @@
-import Color from "./color";
-import IconVariant from "./icon-variant";
-import IconOutline from "./icon-outline";
-
-const Section = { Color, IconVariant, IconOutline };
-
-export default Section;
