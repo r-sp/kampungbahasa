9e424bb9858fa3e1a229789e6a439da07d08a253
r-sp <36327242+r-sp@users.noreply.github.com>
2024-10-16 15:47:46 +0700

    clean demo pages

--- web/app/page.tsx
+++ web/app/page.tsx
@@ -1,5 +1,11 @@
-import ConceptPage from "./concept/page";
-
 export default function Homepage() {
-  return <ConceptPage />;
+  return (
+    <div role="none">
+      <article className="mx-auto flex min-h-svh max-w-screen-lg flex-col items-center justify-center p-4">
+        <h1 className="text-center text-4xl font-bold tracking-tight text-slate-900 md:text-5xl">
+          A place to learn, practice and improve your speaking
+        </h1>
+      </article>
+    </div>
+  );
 }
