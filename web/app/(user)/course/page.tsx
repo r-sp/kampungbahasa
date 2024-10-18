e05deafb8357571475793a92bfc56c3eb07706a4
r-sp <36327242+r-sp@users.noreply.github.com>
2024-10-18 14:10:43 +0700

    add course languages (#72)

--- web/app/(user)/course/page.tsx
+++ web/app/(user)/course/page.tsx
@@ -1,3 +1,45 @@
+import Link from "next/link";
+
 export default function CoursePage() {
-  return <h1 className="text-4xl font-bold sm:text-5xl">Course</h1>;
+  const ItemCourse = (props: { en: string; id: string }) => {
+    const slugify = (lang: string, useLocal: boolean) => {
+      const url = lang.toLowerCase();
+      const slug = useLocal ? `/kursus-bahasa-${url}` : `/${url}-course`;
+      return slug;
+    };
+    const textify = (lang: string, useLocal: boolean) => {
+      const text = useLocal ? `Kursus Bahasa ${lang}` : `${lang} Course`;
+      return text;
+    };
+
+    return (
+      <li key={props.en.toLowerCase()} aria-label={props.en.toLowerCase()}>
+        <Link className="text-slate-700" href={slugify(props.id, true)} lang="id" tabIndex={-1}>
+          {textify(props.id, true)}
+        </Link>
+        <span className="text-slate-500"> / </span>
+        <Link className="text-state-700" href={slugify(props.en, false)} lang="en">
+          {textify(props.en, false)}
+        </Link>
+      </li>
+    );
+  };
+  return (
+    <>
+      <h1 className="text-4xl font-bold sm:text-5xl">Course</h1>
+      <ul className="mt-4 flex flex-col gap-6 text-base">
+        <ItemCourse en="Arabic" id="Arab" />
+        <ItemCourse en="Dutch" id="Belanda" />
+        <ItemCourse en="English" id="Inggris" />
+        <ItemCourse en="French" id="Prancis" />
+        <ItemCourse en="German" id="Jerman" />
+        <ItemCourse en="Indonesia" id="Indonesia" />
+        <ItemCourse en="Japanese" id="Jepang" />
+        <ItemCourse en="Korean" id="Korea" />
+        <ItemCourse en="Mandarin" id="Mandarin" />
+        <ItemCourse en="Russian" id="Rusia" />
+        <ItemCourse en="Spanish" id="Spanyol" />
+      </ul>
+    </>
+  );
 }
