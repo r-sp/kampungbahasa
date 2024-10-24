6fe2b4b9275aad897485e3dbcf6ab70fc9800f0f
r-sp <36327242+r-sp@users.noreply.github.com>
2024-10-24 10:16:26 +0700

    clean component ui

--- web/components/ui/index.ts
+++ web/components/ui/index.ts
@@ -1,7 +1,6 @@
-import ThemeProvider from "./theme-provider";
 import ThemeScript from "./theme-script";
 import ThemeSwitcher from "./theme-switcher";
 import Header from "./header";
 import Footer from "./footer";
 
-export { ThemeProvider, ThemeScript, ThemeSwitcher, Header, Footer };
+export { ThemeScript, ThemeSwitcher, Header, Footer };
