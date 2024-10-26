ab36620dbe13cfe9f41a39d1ea1bde1467fa71c0
r-sp <36327242+r-sp@users.noreply.github.com>
2024-10-26 08:12:15 +0700

    fix layout padding (#90)

--- web/components/ui/header.tsx
+++ web/components/ui/header.tsx
@@ -5,8 +5,8 @@ import Link from "next/link";
 
 export default function Header() {
   return (
-    <header className="p-4">
-      <nav className="mx-auto w-full max-w-screen-lg" role="none">
+    <header>
+      <nav className="mx-auto flex h-12 w-full max-w-screen-lg items-center px-4" role="none">
         <Link className="block size-8" href="/">
           <Logo className="size-8" />
           <span className="sr-only">Kampung Bahasa</span>
