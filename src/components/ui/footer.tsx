ad84817e99bf52e12e14ff99660857621eda2147
r-sp <36327242+r-sp@users.noreply.github.com>
2024-11-08 20:47:31 +0700

    fix theme switcher hydration error

--- src/components/ui/footer.tsx
+++ src/components/ui/footer.tsx
@@ -1,8 +1,10 @@
 "use client";
 
+import dynamic from "next/dynamic";
 import Link from "next/link";
 import { Banner } from "../logo";
-import ThemeSwitcher from "./theme-switcher";
+
+const ThemeSwitcher = dynamic(() => import("./theme-switcher"), { ssr: false });
 
 export default function Footer() {
   return (
